c6700027cf2aa5adc59364603aaa032d25edbb43
Jakub Wilk <kubawilk63@gmail.com>
2025-03-30 22:43:20 +0200

    Process masks along image

--- images_preprocessing.py
+++ images_preprocessing.py
@@ -2,23 +2,31 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from PIL import Image
 
 
 SCALE = 300
 
 images_path = 'datasets/segmentation/original_images/train_set'
-masks_path = 'datasets/segmentation/segmentation_ground_truths/'
+masks_path = 'datasets/segmentation/segmentation_ground_truths/train_set'
 
 
 def pad_image(frame):
+    # Get width and height of frame
     frame_height, frame_width = frame.shape[:2]
+
+    # Assumes that width will be 640 and calculating new height so that image proportion stays the same
     new_height = 640 / frame_width * frame_height
+
+    # Resize image to new shape
     frame = cv2.resize(frame, (640, int(new_height)))
+
     frame_height, frame_width = frame.shape[:2]
     pad_size = 640 - frame_height
     top = pad_size // 2
     bottom = pad_size - top 
     processed_frame = cv2.copyMakeBorder(frame, top, bottom, 0, 0, cv2.BORDER_CONSTANT, None, [0, 0, 0])
+
     return processed_frame, top, bottom
 
 def cut_low_values(frame):
@@ -61,7 +69,7 @@ def circle_detection(frame):
             return frame, (a, b), r
     return frame, None, None
 
-def fundus_fitting(frame):
+def fundus_fitting(frame, masks):
     processed_frame, center, radius = circle_detection(frame.copy())
 
     # Get left and right coordinates of the circle
@@ -75,10 +83,15 @@ def fundus_fitting(frame):
     # Get only the fundus area
     processed_frame = processed_frame[:, min_x:max_x]
 
+    for i, mask in enumerate(masks):
+        mask = mask[:, min_x:max_x]
+        mask, top_pad, bottom_pad = pad_image(mask)
+        masks[i] = mask
+
     # Pad image to 640x640
     processed_frame, top_pad, bottom_pad = pad_image(processed_frame)
 
-    return processed_frame, top_pad, bottom_pad
+    return processed_frame, masks, top_pad, bottom_pad
 
 def color_enhacement(frame):
     processed_frame = frame.copy()
@@ -102,18 +115,43 @@ def mask_area_around_fundus(frame, top_pad, bottom_pad):
     return processed_frame
     
 images = sorted(os.listdir(images_path))
-masks = sorted(os.listdir(masks_path))
+
+# Get dir for every type of mask
+masks_dirs = sorted(os.listdir(masks_path))
+
+# Create a list fo paths for every mask of every type
+masks_list = [sorted(os.listdir(os.path.join(masks_path, masks_dir))) for masks_dir in masks_dirs]
 
 for i in range(len(images)):
     image = cv2.imread(os.path.join(images_path, images[i]))
-    processed_frame, top_pad, bottom_pad = fundus_fitting(image)
+
+    # Read mask of every type for current image
+    image_masks = []
+    for mask_dir_index, dirname in enumerate(masks_dirs):
+        # Read masks
+        mask = np.array(Image.open(os.path.join(masks_path, masks_dirs[mask_dir_index], masks_list[mask_dir_index][i]))) 
+        mask = mask*255 # Rescale from 1 to 255
+        image_masks.append(mask)
+
+    # Fit fundus so that it takes whole image
+    processed_frame, processed_image_masks, top_pad, bottom_pad = fundus_fitting(image, image_masks)
+
+    # Enhance features and colors
     processed_frame = color_enhacement(processed_frame)
+
+    # Mask fundus surrounding to after color enhancement
     processed_frame = mask_area_around_fundus(processed_frame, top_pad, bottom_pad)
     
     if processed_frame is None:
         continue
     
+    # Show processed image
     cv2.imshow('Processed images', processed_frame)
+    
+    # Show masks
+    for mask_index, mask in enumerate(processed_image_masks):
+        cv2.imshow(f"Processed mask {mask_index}", mask)
+
     if cv2.waitKey(0) == ord('q'):
         break
 cv2.destroyAllWindows()
