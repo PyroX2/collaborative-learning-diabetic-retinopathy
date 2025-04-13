6c8a1ab26bc373790a65fe9b05c0101e64a74aca
Jakub Wilk <kubawilk63@gmail.com>
2025-04-13 11:22:53 +0200

    Load images during training to save gpu memory

--- segmentation/dataset.py
+++ segmentation/dataset.py
@@ -10,39 +10,36 @@ class DRSegmentationDataset(Dataset):
 
         images_path = os.path.join(input_path, "images")
 
-        self.images = []
-        for image_path in sorted(os.listdir(images_path)):
-            img = read_image(os.path.join(images_path, image_path))
-            img = (img/255).to(torch.float32)
-            self.images.append(img)
-        self.images = torch.stack(self.images)
+        self.images_paths = [os.path.join(images_path, image_path) for image_path in sorted(os.listdir(images_path))]
 
         if use_masks:
             masks_path = os.path.join(input_path, "masks")
-            self.masks_dirs = sorted(os.listdir(masks_path))
+            self.masks_dirs = {mask_dir: [] for mask_dir in sorted(os.listdir(masks_path))}
 
             self.masks = []
-            for mask_dir in self.masks_dirs:
-                masks = []
+            for mask_dir in self.masks_dirs.keys():
                 for mask_path in sorted(os.listdir(os.path.join(masks_path, mask_dir))):
-                    mask = read_image(os.path.join(masks_path, mask_dir, mask_path))
-                    mask = (mask/255).to(torch.float32)
-                    masks.append(mask)
-                masks = torch.stack(masks)
-                self.masks.append(masks)
-
-            self.masks = torch.stack(self.masks)
+                    mask_path = os.path.join(masks_path, mask_dir, mask_path)
+                    self.masks_dirs[mask_dir].append(mask_path)
 
         self.use_masks = use_masks
 
     def __len__(self):
-        return len(self.images)
+        return len(self.images_paths)
     
     def __getitem__(self, index):
-        input = self.images[index]
+        img = read_image(self.images_paths[index])
+        img = (img/255).to(torch.float32)
+
+        input = img
 
         if self.use_masks:
-            targets = self.masks[:, index].squeeze()
+            masks = []
+            for mask_dir in self.masks_dirs.keys():
+                mask = read_image(self.masks_dirs[mask_dir][index])
+                mask = (mask/255).to(torch.float32).squeeze()
+                masks.append(mask)
+            targets = torch.stack(masks)
         else:
             targets = input
 
