ca905fb5b3a2bf389c71271e39681fbc703900e2
Jakub Wilk <kubawilk63@gmail.com>
2025-08-11 10:21:01 +0200

    In segmentation/test_model.py return and plot overall metrics (without class separation)

--- segmentation/test_model.py
+++ segmentation/test_model.py
@@ -46,8 +46,8 @@ def calculate_mask_metrics(validation_px_dataloader, generator_model, criterion,
     all_metrics = []
     for i in range(NUM_CLASSES):
         class_metrics = {
-            "class_name": class_names[i],
-            "loss": validation_loss[i]        
+            'class_name': class_names[i],
+            'loss': validation_loss[i]        
         }
         class_targets = torch.tensor(targets[i]).to(device)
         class_predicted_values = torch.tensor(predicted_values[i]).to(device)
@@ -68,11 +68,22 @@ def calculate_mask_metrics(validation_px_dataloader, generator_model, criterion,
         f1_metric.update(class_predicted_values, class_targets)
         f1_score = f1_metric.compute()
 
-        class_metrics["auroc"] = auroc.item()
-        class_metrics["auprc"] = auprc.item()
-        class_metrics["accuracy"] = accuracy.item()
-        class_metrics["f1_score"] = f1_score.item()
+        class_metrics['auroc'] = auroc.item()
+        class_metrics['auprc'] = auprc.item()
+        class_metrics['accuracy'] = accuracy.item()
+        class_metrics['f1_score'] = f1_score.item()
         all_metrics.append(class_metrics)
+
+    # Mean value of metric for all classes 
+    validation_epoch_loss = np.mean(validation_loss)
+    val_auroc = np.mean([metrics['auroc'] for metrics in all_metrics])
+    val_auprc = np.mean([metrics['auprc'] for metrics in all_metrics])
+    val_accuracy = np.mean([metrics['accuracy'] for metrics in all_metrics])
+    val_f1_score = np.mean([metrics['f1_score'] for metrics in all_metrics])
+
+
+    return all_metrics, validation_epoch_loss, val_auroc, val_auprc, val_accuracy, val_f1_score
+
 
 test_dataset = DRSegmentationDataset(test_dataset_dir)
 
@@ -89,7 +100,7 @@ loss = torch.nn.BCELoss()
 
 class_names = test_dataset.class_names
 
-metrics = calculate_mask_metrics(test_dataloader, loaded_model, loss, class_names)
+metrics, validation_epoch_loss, val_auroc, val_auprc, val_accuracy, val_f1_score = calculate_mask_metrics(test_dataloader, loaded_model, loss, class_names)
 
 metrics_names = ['loss', 'auroc', 'auprc', 'accuracy', 'f1_score']
 
@@ -112,5 +123,24 @@ for i, metric in enumerate(metrics_names):
             va='bottom',
             fontsize=10
         )
+
+plt.subplot(2, 3, 6)
+values = [validation_epoch_loss, val_auroc, val_auprc, val_accuracy, val_f1_score]
+bars = plt.bar(['Test Loss', 'Test AUROC', 'Test AUPRC', 'Test Accuracy', 'Test F1 Score'], values)
+plt.title('Overall Metrics')
+plt.xticks(rotation=45)
+plt.ylabel('Value')
+plt.ylim(0, 1.2)
+
+for bar, value in zip(bars, values):
+    plt.text(
+        bar.get_x() + bar.get_width() / 2,
+        bar.get_height(),
+        f'{value:.3f}',
+        ha='center',
+        va='bottom',
+        fontsize=10
+    )
+
 plt.tight_layout()
 plt.savefig('segmentation_metrics.png')
