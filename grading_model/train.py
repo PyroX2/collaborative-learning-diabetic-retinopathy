b7a6b85c72a745a29345dc9de85db8ecb98a0319
Jakub Wilk <kubawilk63@gmail.com>
2025-08-16 11:25:09 +0200

    Adapt attentive model training script for binary classification

--- grading_model/train.py
+++ grading_model/train.py
@@ -1,9 +1,7 @@
-from grading_model.dataset import GradingDataset
 from grading_model.grading_model import GradingModel
 import torch
-from torch import nn
 from torch.utils.data import DataLoader, random_split
-from torcheval.metrics import MulticlassAccuracy, MulticlassAUPRC, MulticlassAUROC, MulticlassF1Score
+from torcheval.metrics import BinaryAccuracy, BinaryAUPRC, BinaryAUROC, BinaryF1Score
 from torch.utils.tensorboard import SummaryWriter
 import cv2
 from tqdm import tqdm
@@ -13,6 +11,7 @@ from segmentation.unet import UNet
 import os
 import mlflow
 from mlflow.models import infer_signature
+import torch.nn.functional as F
 
 
 torch.manual_seed(0)
@@ -23,11 +22,14 @@ TENSORBOARD = True
 LOG_NAME = "attentive_grading_model_train"
 NUM_EPOCHS = 100
 
-OPTIMIZER_STATE_DICT = ''
-GRADING_MODEL_STATE_DICT = ''
-SEGMENTATION_MODEL_STATE_DICT = ''
-CHECKPOINT_DIR = ''
-DATASET_PATH = ''
+NUM_LESIONS = 4
+NUM_OUTPUTS = 1 # Number of outputs in grading model. 1 when used for binary classification
+
+OPTIMIZER_STATE_DICT = '/users/scratch1/s189737/collaborative-learning-diabetic-retinopathy/models/checkpoints/classification/grading_model_pretrain_two_classes_optimizer_best.pth'
+GRADING_MODEL_STATE_DICT = '/users/scratch1/s189737/collaborative-learning-diabetic-retinopathy/models/checkpoints/classification/grading_model_pretrain_two_classes_best.pth'
+SEGMENTATION_MODEL_STATE_DICT = '/users/scratch1/s189737/collaborative-learning-diabetic-retinopathy/models/segmentation/segmentation_generator.pth'
+CHECKPOINT_DIR = '/users/scratch1/s189737/collaborative-learning-diabetic-retinopathy/models/checkpoints/classification/attentive_model_train'
+DATASET_PATH = '/users/scratch1/s189737/collaborative-learning-diabetic-retinopathy/datasets/eyepacs-aptos-messidor-diabetic-retinopathy-original-preprocessed-color-enhancement'
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -46,27 +48,28 @@ def validate(grading_model, grading_model_pretrained, segmentation_model, valida
         with torch.no_grad():
             for batch_index, (input_batch, target_batch) in tqdm(enumerate(validation_dataloader)):
                 input_batch = input_batch.to(device)
-                target_batch = target_batch.to(device)
+                target_batch = target_batch.to(device).to(torch.float32)
 
                 masks = segmentation_model(input_batch)
+                masks = torch.cat((masks[:, :3], masks[:, 4:]), dim=1) # Drop optic disc if applicable
                 pretrained_logits, pretrained_f_low, pretrained_f_high, _ = grading_model_pretrained(input_batch)
 
                 logits, _, _, attention_maps = grading_model(input_batch, masks, pretrained_f_low, pretrained_f_high)
+                output = F.sigmoid(logits).squeeze(-1)
 
                 if epoch is not None and epoch % 1 == 0 and batch_index == 0:
                     reference_image = input_batch[0].cpu().detach().numpy()
                     reference_image = reference_image.transpose(1, 2, 0)
 
                     # TODO: Add min max scaling
-                    reference_image = (reference_image - reference_image.min()) / (reference_image.max() - reference_image.min())
                     reference_image = (reference_image * 255).astype('uint8')
                     if not os.path.exists("output_masks"):
                         os.makedirs("output_masks")
                     cv2.imwrite("output_masks/reference_image.png", reference_image)
                     
-                    for i in range(5):
+                    for i in range(NUM_LESIONS):
                         attention_map = attention_maps[0, i].cpu().detach().numpy()
-                        attention_map = (attention_map - attention_map.min()) / (attention_map.max() - attention_map.min())
+                        print(f"{attention_map.min()}, {attention_map.max()}")
                         attention_map = (attention_map * 255).astype('uint8')
 
                         mask = masks[0][i].cpu().detach().numpy()
@@ -78,10 +81,9 @@ def validate(grading_model, grading_model_pretrained, segmentation_model, valida
                         cv2.imwrite(os.path.join(output_path, f"mask_{epoch}.png"), attention_map)
                         cv2.imwrite(os.path.join(output_path, f"mask_generator.png"), mask)
                 
-                loss = criterion(logits, target_batch)
-                normalized_output = torch.softmax(logits, dim=-1)
+                loss = criterion(output, target_batch)
 
-                predicted_values.extend(normalized_output.cpu().detach().tolist())
+                predicted_values.extend(output.cpu().detach().tolist())
                 
                 
                 if len(target_batch) > 1:
@@ -91,7 +93,7 @@ def validate(grading_model, grading_model_pretrained, segmentation_model, valida
 
                 validation_loss += loss.detach().item()
 
-                del input_batch, target_batch, masks, logits, attention_maps, loss, normalized_output
+                del input_batch, target_batch, masks, logits, attention_maps, loss, output
                 torch.cuda.empty_cache()
 
         mean_validation_loss = validation_loss / len(validation_dataloader)
@@ -99,19 +101,19 @@ def validate(grading_model, grading_model_pretrained, segmentation_model, valida
         predicted_values = torch.tensor(predicted_values)
         targets = torch.tensor(targets)
 
-        f1_metric = MulticlassF1Score(num_classes=5, average='macro')
+        f1_metric = BinaryF1Score()
         f1_metric.update(predicted_values, targets)
         f1_score = f1_metric.compute()
 
-        accuracy_metric = MulticlassAccuracy(num_classes=5, average='macro')
+        accuracy_metric = BinaryAccuracy()
         accuracy_metric.update(predicted_values, targets)
         accuracy_score = accuracy_metric.compute()
         
-        auprc_metric = MulticlassAUPRC(num_classes=5, average='macro')
+        auprc_metric = BinaryAUPRC()
         auprc_metric.update(predicted_values, targets)
         auprc_score = auprc_metric.compute()
 
-        auroc_metric = MulticlassAUROC(num_classes=5, average='macro')
+        auroc_metric = BinaryAUROC()
         auroc_metric.update(predicted_values, targets)
         auroc_score = auroc_metric.compute()
 
@@ -119,7 +121,6 @@ def validate(grading_model, grading_model_pretrained, segmentation_model, valida
 
 def train(grading_model, grading_model_pretrained, segmentation_model, train_dataloader, train_metrics_dataloader, validation_dataloader, optimizer, criterion, n_epochs):
     best_validation_loss = float("inf")
-    best_model_state_dict = None
     for epoch in range(n_epochs):
         epoch_loss = 0
 
@@ -130,19 +131,21 @@ def train(grading_model, grading_model_pretrained, segmentation_model, train_dat
             optimizer.zero_grad()
 
             input_batch = input_batch.to(device)
-            target_batch = target_batch.to(device)
+            target_batch = target_batch.to(device).to(torch.float32)
 
             with torch.no_grad():
                 masks = segmentation_model(input_batch)
                 pretrained_logits, pretrained_f_low, pretrained_f_high, _ = grading_model_pretrained(input_batch)
                 
                 masks = masks.detach()
+                masks = torch.cat((masks[:, :3], masks[:, 4:]), dim=1) # Drop optic disc if applicable
                 pretrained_f_low = pretrained_f_low.detach()
                 pretrained_f_high = pretrained_f_high.detach()
 
             logits, _, _, attention_maps = grading_model(input_batch, masks, pretrained_f_low, pretrained_f_high)
+            output = F.sigmoid(logits).squeeze(-1)
 
-            loss = criterion(logits, target_batch)
+            loss = criterion(output, target_batch)
 
             epoch_loss += loss.detach().item()
 
@@ -154,6 +157,7 @@ def train(grading_model, grading_model_pretrained, segmentation_model, train_dat
         del masks
         del logits
         del attention_maps
+        del output
         torch.cuda.empty_cache()
         
         _, train_accuracy_score, train_f1_score, train_auprc_score, train_auroc_score = validate(grading_model, grading_model_pretrained, segmentation_model, train_metrics_dataloader, criterion)
@@ -189,8 +193,7 @@ def train(grading_model, grading_model_pretrained, segmentation_model, train_dat
         if mean_validation_loss < best_validation_loss:
             best_validation_loss = mean_validation_loss
             torch.save(grading_model.state_dict(), os.path.join(CHECKPOINT_DIR, f"{LOG_NAME}_best.pth"))
-            best_model_state_dict = grading_model.state_dict()
-            
+
         print(f"Epoch: {epoch}, Mean training loss: {mean_training_loss}, Mean validation loss: {mean_validation_loss}")
 
     torch.save(grading_model.state_dict(), os.path.join(CHECKPOINT_DIR, f"{LOG_NAME}_final.pth"))
@@ -213,26 +216,23 @@ def main():
         v2.Resize((640, 640))])
 
     # Kaggle Joined dataset
-    train_root = os.path.join({DATASET_PATH}, "train")
-    validation_root = os.path.join({DATASET_PATH}, "val")
-    test_root = os.path.join({DATASET_PATH}, "test")
+    train_root = os.path.join(DATASET_PATH, "train", "two_classes")
+    validation_root = os.path.join(DATASET_PATH, "val", "two_classes")
 
     train_dataset = ImageFolder(train_root, transform=transform)
     validation_dataset = ImageFolder(validation_root, transform=test_images_transform)
-    test_dataset = ImageFolder(test_root, transform=test_images_transform)
 
     _, train_metrics_dataset = random_split(train_dataset, [0.9, 0.1])
 
     train_dataloader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True, num_workers=42)
     validation_dataloader = DataLoader(validation_dataset, BATCH_SIZE, shuffle=False, num_workers=42)
-    test_dataloader = DataLoader(test_dataset, BATCH_SIZE, shuffle=False, num_workers=42)
     train_metrics_dataloader = DataLoader(train_metrics_dataset, BATCH_SIZE, shuffle=False, num_workers=42)
 
-    grading_model_pretrained = GradingModel()
+    grading_model_pretrained = GradingModel(num_lesions=NUM_LESIONS, num_outputs=NUM_OUTPUTS)
     grading_model_pretrained.to(device)
     grading_model_pretrained.load_state_dict(torch.load(GRADING_MODEL_STATE_DICT, weights_only=True, map_location=device))
 
-    grading_model = GradingModel()
+    grading_model = GradingModel(num_lesions=NUM_LESIONS, num_outputs=NUM_OUTPUTS)
     grading_model.to(device)
     grading_model.load_state_dict(torch.load(GRADING_MODEL_STATE_DICT, weights_only=True, map_location=device))
 
@@ -243,15 +243,16 @@ def main():
     optimizer = torch.optim.Adam(grading_model.parameters(), lr=1e-5)
     optimizer.load_state_dict(torch.load(OPTIMIZER_STATE_DICT, map_location=device))
 
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.BCELoss()
         
     if MLFLOW:
+        mlflow.set_tracking_uri("http://localhost:5000")
         mlflow.set_experiment("attentive-model-training")
         with mlflow.start_run(run_name=LOG_NAME):
             train(grading_model, grading_model_pretrained, segmentation_model, train_dataloader, train_metrics_dataloader, validation_dataloader, optimizer, criterion, NUM_EPOCHS)
 
             # Obtain data for creating signatures for models logging into mlflow
-            x_test, y_test = next(iter(test_dataloader))
+            x_test, y_test = next(iter(validation_dataloader))
             x_test = x_test.to(device)
 
             with torch.no_grad():
